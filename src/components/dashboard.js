47aac4273917976d38bc9f7b239b6cbe560295b6
Anurag <anuragagrawal4998@gmail.com>
2020-09-26 18:53:49 +0530

    dashboardlet

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import getTasks from "../apicall/gettasks";
 import AddTask from "./addTaskbBar"
-let a;
 const Dashboard = () => {
 
   const [tasks, setTasks] = useState([]);
   useEffect(() => {
     async function fetchtasks() {
-      a = await getTasks();
+     let a = await getTasks();
       setTasks(a)
     };
     fetchtasks()
