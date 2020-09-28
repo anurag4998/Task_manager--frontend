54dcdc14cd8eba0999508fcd492f9c2f489618ab
Anurag <anuragagrawal4998@gmail.com>
2020-09-28 14:26:02 +0530

    time formatting

--- src/components/table.js
+++ src/components/table.js
@@ -7,18 +7,22 @@ const Table = (props) => {
         async function fetchtasks() {
             let a = await getTasks();
             setTasks(a)
-
         };
         fetchtasks()
     }, [props.taskarray])
 
     let tasklist = tasks.map(task => task.description)
     let completed = tasks.map(task => task.completed)
-    let createdAt = tasks.map(task => task.createdAt)
+    let createdAt = tasks.map(task => {
+        let localDate = new Date(task.createdAt)
+        let event = new Date(localDate)
+        return `${event.getDate()} ${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(event)} ${event.toLocaleTimeString('en-US')}`
+    })
+
+
     return (
         <div>
-            <h1>{tasklist}</h1>
-            <h1>{createdAt}</h1>
+
         </div>
     )
 }
